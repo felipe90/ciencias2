2c910bd98cb547294eb91687ac39e3ec30491f6d
AFelipe90 <lordganondorf@gmail.com>
2015-02-06 20:17:21 -0500

    agregada funcion initArray

--- arbolAVL/functions.js
+++ arbolAVL/functions.js
@@ -1,18 +1,29 @@
-var textoNodo;
+var textNode;
 
-var nodo={
+var node={
 	id:0,
 	content:0,
-	izq:0,
-	der:0,
-	facVal:0;
+	lefh:0,
+	rigth:0,
+	valFact:0
 } 
 
+var arregloNodos=[];
+
+function initArray () {
+	nodeAux=node;
+	node.id=1;
+	arregloNodos[0]=nodeAux;	
+}
 
 $(function () {
 
+	initArray(); //init cursor array
+
+	console.log(arregloNodos[0].id);
+
 	$("#botonAgregar").on("click",function(){
-		textoNodo=$("#numeroTexto").val();
+		textNodo=$("#numeroTexto").val();
 		
 	});
 
