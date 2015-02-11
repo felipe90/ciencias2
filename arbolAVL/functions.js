10b69840a37ccc3eb57c2c109dfcdbc441e62011
felipe90 <lordganondorf@gmail.com>
2015-02-11 06:38:11 -0500

    cambios antes de agregar funcionnalidad (tabla)

--- arbolAVL/functions.js
+++ arbolAVL/functions.js
@@ -1,4 +1,5 @@
 var textNode;
+var nNodos=10;
 
 var node={
 	id:0,
@@ -11,20 +12,45 @@ var node={
 var arregloNodos=[];
 
 function initArray () {
+
+	//numero de nodos iniciales nNodos
+
 	nodeAux=node;
 	node.id=1;
-	arregloNodos[0]=nodeAux;	
+
+	for(var i=0;i<nNodos;i++){
+		arregloNodos[i]=nodeAux;
+		arregloNodos[i].id=i;
+	}
+	
+}
+
+function drawArray () {
+	//var html = "<table><tr><td>hello</td></<tr></table>";
+	var html = "<table border=1><tr><td>Registro</td><td>Nombre</td><td>Izquierda</td><td>Derecha</td><td>Factor de valace</td></tr</table>";
+/*
+	for(var i=0;i<nNodos;i++){
+		html = html+"";
+	}
+
+	html = html+"<table><tr><td>hello</td></<tr></table>";
+	*/
+	$("#resultado").html( html );
 }
 
 $(function () {
 
 	initArray(); //init cursor array
 
-	console.log(arregloNodos[0].id);
+	console.log(arregloNodos.length);
 
 	$("#botonAgregar").on("click",function(){
 		textNodo=$("#numeroTexto").val();
 		
+	});
+
+	$("#botonResultado").on("click",function(){
+		drawArray();
 	});
 
 
