bd64ccc7cdfc237773ad036d695f030ec4e5e49d
AFelipe90 <lordganondorf@gmail.com>
2015-02-11 12:43:41 -0500

    camcbios hechos en clase

--- arbolAVL/functions.js
+++ arbolAVL/functions.js
@@ -1,30 +1,57 @@
 var textNode;
+var nNodos=10;
 
-var node={
-	id:0,
-	content:0,
-	lefh:0,
-	rigth:0,
-	valFact:0
+//defincion de la clase
+function node(id,content,left,right,valFact){
+	this.id=id;
+	this.content=content;
+	this.lefth=left;
+	this.rigth=right;
+	this.valFact=valFact;
 } 
 
 var arregloNodos=[];
 
 function initArray () {
-	nodeAux=node;
-	node.id=1;
-	arregloNodos[0]=nodeAux;	
+
+	//numero de nodos iniciales nNodos
+	
+	for(var i=0;i<nNodos;i++){
+		arregloNodos[i]=new node(i,0,0,0,0);
+	}
+	
+}
+
+function drawArray () {
+	//var html = "<table><tr><td>hello</td></<tr></table>";
+	var html = "<table border=1><tr><td>Registro</td><td>Nombre</td><td>Izquierda</td><td>Derecha</td><td>Factor de valace</td></tr>";
+
+	for(var i=0;i<arregloNodos.length;i++){
+		html = html+"<tr><td>"+arregloNodos[i].id+"</td>";
+		html = html+"<td>"+arregloNodos[i].content+"</td>";
+		html = html+"<td>"+arregloNodos[i].lefth+"</td>";
+		html = html+"<td>"+arregloNodos[i].rigth+"</td>";
+		html = html+"<td>"+arregloNodos[i].valFact+"</td>";
+		html = html+"</tr>";
+	}
+
+	html = html+"</table>";
+	
+	$("#resultado").html( html );
 }
 
 $(function () {
 
 	initArray(); //init cursor array
 
-	console.log(arregloNodos[0].id);
-
 	$("#botonAgregar").on("click",function(){
 		textNodo=$("#numeroTexto").val();
+		console.log(textNodo);
 		
+	});
+
+	$("#botonResultado").on("click",function(){
+		drawArray();
 	});
 
 
