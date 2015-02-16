5a5fbf06dffc927e5b0515320434c65950d5498c
felipe90 <lordganondorf@gmail.com>
2015-02-15 21:25:10 -0500

    inicio proyecto huffman

---
+++ Huffman/functions.js
@@ -0,0 +1,12 @@
+
+
+//funcion pricipal, se ejecuta luego de cargar la pagina
+$(function () {
+	//funcion para agregar nodos
+	$("#botonResultado").on("click",function(){
+		console.log("huffman");
+	});
+
+});
+
+
