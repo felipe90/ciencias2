474d05ee1f5178cd3ef1724e2a6e470a4527283a
felipe90 <lordganondorf@gmail.com>
2015-02-17 12:46:03 -0500

    agrega la clase huffman y el constructor

--- Huffman/functions.js
+++ Huffman/functions.js
@@ -1,3 +1,81 @@
+var huffmanArray=new Array();
+var menssage;
+var huffmanObj;
+
+function huffman (menssage) {
+	var huffmanArrayLength;
+
+	//huffman rows
+	var SIMBOL=0;
+	var FRECUENCY=1;
+	var FATHER=2;
+	var TYPE=3;
+	var LEFT=4;
+	var RIGHT=5;
+
+	constructor=huffmanConst(menssage);
+
+}
+
+function huffmanConst (menssage) {
+		var i,j;
+
+		var temp=[];
+		for (var i = 0; i < 26; i++) {
+			temp[i]=0;
+		}
+
+		for (var i = 0; i < menssage.length; i++) {
+			temp[menssage.charAt(i) - 97]++;
+		}
+
+		var huffmanArrayLength=0;
+
+		for (var i = 0; i < 26; i++) {
+			if (temp[i]!=0) {
+				huffmanArrayLength++;
+			}
+		}
+
+		var numberCol= huffmanArrayLength*2-1;
+
+		for (var i = 0; i < 6; i++) {
+			for (var i = 0; i < numberCol; i++) {
+				huffmanArray[i]=new Array();
+				huffmanArray[i][j] = 0;
+			}
+		}
+
+		var j=0;
+		for (var i = 0; i < 26; i++) {
+			if (temp[i] != 0) {
+				huffmanArray[SIMBOL][j]=i; //letter
+				huffmanArray[FRECUENCY][j]=temp[i]; //frecuenci
+				j++;
+			}
+		}
+}
+
+function decode (argument) {
+	// body...
+}
+
+function code (argument) {
+	// body...
+}
+
+function numberSimbols (argument) {
+	// body...
+}
+
+function listArray (menor) {
+	// body...
+}
+
+function int (argument) {
+	// body...
+}
+
 
 
 //funcion pricipal, se ejecuta luego de cargar la pagina
@@ -5,6 +83,8 @@ $(function () {
 	//funcion para agregar nodos
 	$("#botonResultado").on("click",function(){
 		console.log("huffman");
+		textNodo=$("#numeroTexto").val();
+		huffmanObj=new huffman(textNodo);
 	});
 
 });
